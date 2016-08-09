95abcc7a40a2c595fc08f081b3a0ac000eec38d9
pxai <pello_altadill@cuatrovientos.org>
2016-08-09 14:58:46 +0200

    Chapter 1 done

---
+++ app/src/main/java/org/cuatrovientos/android/shunshine/CheckAsyncTask.java
@@ -0,0 +1,10 @@
+package org.cuatrovientos.android.shunshine;
+
+/**
+ * Pello Altadill
+ * API_KEY=776cb70718be419490efbf98d4cc5125
+ * Sample: http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={API_KEY}
+ * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&APPID=776cb70718be419490efbf98d4cc5125
+ */
+public class CheckAsyncTask {
+}
