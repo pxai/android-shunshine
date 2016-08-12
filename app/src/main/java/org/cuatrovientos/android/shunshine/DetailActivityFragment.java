ff815eb7c2fbf6c7c7a40ad66af45a62fbc066d9
pxai <pello_altadill@cuatrovientos.org>
2016-08-12 09:50:18 +0200

    Added settings activity

--- app/src/main/java/org/cuatrovientos/android/shunshine/DetailActivityFragment.java
+++ app/src/main/java/org/cuatrovientos/android/shunshine/DetailActivityFragment.java
@@ -1,8 +1,13 @@
 package org.cuatrovientos.android.shunshine;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,10 +22,45 @@ public class DetailActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         detailTextView = (TextView) rootView.findViewById(R.id.textview_detail);
+
+        // Get data from intent
+        Intent intent = getActivity().getIntent();
+        if (null != intent && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            detailTextView.setText(forecastStr);
+        } else {
+            detailTextView.setText("No data");
+        }
+
         return rootView;
     }
 }
