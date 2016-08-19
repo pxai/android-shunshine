f89fb53db0e9484a237e8167ee6789ccd3ffdc83
pxai <pello_altadill@cuatrovientos.org>
2016-08-19 10:10:56 +0200

    added preferences

--- app/src/main/java/org/cuatrovientos/android/shunshine/SettingsActivity.java
+++ app/src/main/java/org/cuatrovientos/android/shunshine/SettingsActivity.java
@@ -191,7 +191,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // updated to reflect the new value, per the Android Design
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("example_text"));
-            bindPreferenceSummaryToValue(findPreference("example_list"));
+            bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("postal_code"));
         }
 
         @Override
